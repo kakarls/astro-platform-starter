e1197c3c3b337d35372257ce34707f8d170c64fe
kakarls <kacikarlsson@gmail.com>
2026-04-10 19:46:31 -0300

    Update astro.config.mjs
    Explicitly stating server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+    output: 'server',
     vite: {
         plugins: [tailwindcss()]
     },
